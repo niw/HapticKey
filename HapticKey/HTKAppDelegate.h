3dbc7e4682dcd29b5111325705e5172dad9d441a
Yoshimasa Niwa <niw@niw.at>
2017-12-14 22:47:15 -0800

    Update app delegate.

--- HapticKey/HTKAppDelegate.h
+++ HapticKey/HTKAppDelegate.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface HTKAppDelegate : NSObject <NSApplicationDelegate>
+@interface HTKAppDelegate : NSObject
 
 @end
 
--- HapticKey/HTKAppDelegate.m
+++ HapticKey/HTKAppDelegate.m
@@ -9,30 +9,113 @@
 #import "HTKAppDelegate.h"
 #import "HTKHapticFeedback.h"
 #import "HTKFunctionKeyEventListener.h"
+#import "HTKTapGestureEventListener.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface HTKAppDelegate ()
+static NSString * const kListeningEventTypeUserDefaultsKey = @"ListeningEventType";
+
+typedef NS_ENUM(NSUInteger, HTKAppDelegateListeningEventType) {
+    HTKAppDelegateListeningEventTypeFunctionKey,
+    HTKAppDelegateListeningEventTypeTapGesture
+};
+
+@interface HTKAppDelegate () <NSApplicationDelegate>
+
+@property (nonatomic, getter=isFinishedLaunching) BOOL finishedLaunching;
+
+@property (nonatomic) HTKAppDelegateListeningEventType listeningEventType;
+
+@property (nonatomic, nullable) HTKHapticFeedback *hapticFeedback;
 
 @property (nonatomic, nullable) NSStatusItem *statusItem;
-@property (nonatomic, nullable) HTKHapticFeedback *hapticFeedback;
+
+@property (nonatomic, nullable) NSMenuItem *functionKeyEventTypeMenuItem;
+@property (nonatomic, nullable) NSMenuItem *tapGestureEventTypeMenuItem;
 
 @end
 
 @implementation HTKAppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+- (void)setListeningEventType:(HTKAppDelegateListeningEventType)listeningEventType
 {
-    [self _htk_main_loadStatusItem];
+    if (_listeningEventType != listeningEventType) {
+        _listeningEventType = listeningEventType;
 
-    if (AXIsProcessTrusted()) {
-        [self _htk_main_loadHapticFeedback];
-    } else {
-        [self _htk_main_presentAccessibilityPermissionAlert];
+        [self _htk_main_updateStatusItemMenuItems];
+        [self _htk_main_updateHapticFeedback];
+
+        [self _htk_main_preserveUserDefaults];
     }
 }
 
-- (void)_htk_main_presentAccessibilityPermissionAlert
+- (void)_htk_main_updateStatusItemMenuItems
+{
+    if (!self.finishedLaunching) {
+        return;
+    }
+
+    self.functionKeyEventTypeMenuItem.state = (self.listeningEventType == HTKAppDelegateListeningEventTypeFunctionKey) ? NSControlStateValueOn : NSControlStateValueOff;
+    self.tapGestureEventTypeMenuItem.state = (self.listeningEventType == HTKAppDelegateListeningEventTypeTapGesture) ? NSControlStateValueOn : NSControlStateValueOff;
+}
+
+- (void)_htk_main_updateHapticFeedback
+{
+    if (!self.finishedLaunching) {
+        return;
+    }
+
+    if (!AXIsProcessTrusted()) {
+        return;
+    }
+
+    HTKEventListener *eventListener;
+    switch (self.listeningEventType) {
+        case HTKAppDelegateListeningEventTypeFunctionKey:
+            eventListener = [[HTKFunctionKeyEventListener alloc] init];
+            break;
+        case HTKAppDelegateListeningEventTypeTapGesture:
+            eventListener = [[HTKTapGestureEventListener alloc] init];
+            break;
+    }
+    HTKHapticFeedback * const hapticFeedback = [[HTKHapticFeedback alloc] initWithEventListener:eventListener];
+    hapticFeedback.enabled = YES;
+    self.hapticFeedback = hapticFeedback;
+}
+
+// MARK: - User defaults
+
+- (void)_htk_main_preserveUserDefaults
+{
+    NSUserDefaults * const defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setInteger:self.listeningEventType forKey:kListeningEventTypeUserDefaultsKey];
+}
+
+- (void)_htk_main_restoreUserDefaults
+{
+    NSUserDefaults * const defaults = [NSUserDefaults standardUserDefaults];
+    self.listeningEventType = [defaults integerForKey:kListeningEventTypeUserDefaultsKey];
+}
+
+
+// MARK: - NSApplicationDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    [self _htk_main_restoreUserDefaults];
+    [self _htk_main_loadStatusItem];
+
+    self.finishedLaunching = YES;
+
+    [self _htk_main_updateStatusItemMenuItems];
+    [self _htk_main_updateHapticFeedback];
+
+    if (!AXIsProcessTrusted()) {
+        [self _htk_main_presentAccessibilityPermissionAlertAndTerminate];
+    }
+}
+
+- (void)_htk_main_presentAccessibilityPermissionAlertAndTerminate
 {
     NSAlert * const alert = [[NSAlert alloc] init];
     [alert addButtonWithTitle:@"OK"];
@@ -56,6 +139,29 @@ NS_ASSUME_NONNULL_BEGIN
 
     NSMenu * const statusMenu = [[NSMenu alloc] init];
 
+    NSMenu * const eventTypeMenu = [[NSMenu alloc] init];
+
+    NSMenuItem * const functionKeyEventTypeMenuItem = [[NSMenuItem alloc] init];
+    functionKeyEventTypeMenuItem.title = @"ESC and Fn1, Fn2, etc. Keys";
+    functionKeyEventTypeMenuItem.action = @selector(_htk_action_didSelectModeMenuItem:);
+    functionKeyEventTypeMenuItem.target = self;
+    [eventTypeMenu addItem:functionKeyEventTypeMenuItem];
+    self.functionKeyEventTypeMenuItem = functionKeyEventTypeMenuItem;
+
+    NSMenuItem * const tapGestureEventTypeMenuItem = [[NSMenuItem alloc] init];
+    tapGestureEventTypeMenuItem.title = @"All taps on Touch Bar";
+    tapGestureEventTypeMenuItem.action = @selector(_htk_action_didSelectModeMenuItem:);
+    tapGestureEventTypeMenuItem.target = self;
+    [eventTypeMenu addItem:tapGestureEventTypeMenuItem];
+    self.tapGestureEventTypeMenuItem = tapGestureEventTypeMenuItem;
+
+    NSMenuItem * const listeningEventTypeMenuItem = [[NSMenuItem alloc] init];
+    listeningEventTypeMenuItem.title = @"Event";
+    listeningEventTypeMenuItem.submenu = eventTypeMenu;
+    [statusMenu addItem:listeningEventTypeMenuItem];
+
+    [statusMenu addItem:[NSMenuItem separatorItem]];
+
     NSMenuItem * const quitMenuItem = [[NSMenuItem alloc] init];
     quitMenuItem.title = @"Quit";
     quitMenuItem.keyEquivalent = @"q";
@@ -68,16 +174,20 @@ NS_ASSUME_NONNULL_BEGIN
     self.statusItem = statusItem;
 }
 
-- (void)_htk_main_loadHapticFeedback
-{
-    HTKHapticFeedback * const hapticFeedback = [[HTKHapticFeedback alloc] initWithEventListener:[[HTKFunctionKeyEventListener alloc] init]];
-    hapticFeedback.enabled = YES;
-    self.hapticFeedback = hapticFeedback;
-}
-
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
 {
     return NO;
+}
+
+// MARK: - Actions
+
+- (void)_htk_action_didSelectModeMenuItem:(id)sender
+{
+    if (sender == self.functionKeyEventTypeMenuItem) {
+        self.listeningEventType = HTKAppDelegateListeningEventTypeFunctionKey;
+    } else if (sender == self.tapGestureEventTypeMenuItem) {
+        self.listeningEventType = HTKAppDelegateListeningEventTypeTapGesture;
+    }
 }
 
 @end
